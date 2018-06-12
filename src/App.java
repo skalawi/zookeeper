e17e737eb2259b532ab8730d1f6be022337e1431
skalawi <skalawiktor@gmail.com>
2018-06-12 12:26:27 +0200

    added basic classes

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+
+    //TODO Add zookeeper connection
+
+    //TODO Add watcher for znode_testowy
+
+    //TODO Add startup of TestApp on znode create
+
+    //TODO Add closing TestApp on znode delete
+}
---
+++ src/TestApp.java
@@ -0,0 +1,6 @@
+public class TestApp {
+    public static void main(String[] args){
+        System.out.println("Hello");
+        //TODO Graphic representation of watching znode children
+    }
+}
